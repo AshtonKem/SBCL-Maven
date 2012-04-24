56de74ea22e234b54c6c754b5260a15a4b4d3686
Ashton Kemerling <ashtonkemerling@gmail.com>
2012-04-23 23:10:33 -0500

    Add implementation for SourceLayout, called StandardLayout. Update pom dependencies

---
+++ src/main/java/com/github/ashtonkem/StandardLayout.java
@@ -0,0 +1,47 @@
+package com.github.ashtonkem;
+
+import java.io.File;
+import org.apache.maven.project.MavenProject;
+
+/**
+ * Represents the standard lisp project layout. Files will be located as such:
+ * 
+ * <pre>
+ *  BASE
+ *    +-- doc/overview.edoc
+ *    +-- src/lisp (*.lisp)
+ *    +-- lib/lisp (*.lisp)
+ *    +-- test/lisp (*.lisp)
+ *    +-- target (build artifacts)
+ *    +-- target/fasl (*.fasl files)
+ *    +-- pom.xml
+ * </pre>
+ * 
+ * @author ashtonkemerling
+ * 
+ */
+public class StandardLayout implements SourceLayout {
+	private File base;
+	
+	public StandardLayout(MavenProject project)
+	{
+		base = project.getBasedir();
+	}
+
+	public File base() {
+		return base;
+	}
+
+	public File src() {
+		return new File(base, "src/lisp");
+	}
+
+	public File lib() {
+		return new File(base, "lib/lisp");
+	}
+
+	public File test() {
+		return new File(base, "test");
+	}
+
+}
