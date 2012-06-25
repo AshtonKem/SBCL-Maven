f53545025404cf9a97baccb3220f6752bb6c8962
Ashton Kemerling <ashtonkemerling@gmail.com>
2012-06-25 16:56:37 -0500

    Change Command around a bit. It's now a bit more sensitive to ordering of inputs, but that's OK, since the lisp repl is also sensitive to ordering. We're going to phase out the getCommand functionality in a bit, the Iterable<String> takes care of this way better, and avoids issues like referencing packages before they've been added

--- src/main/java/com/github/ashtonkem/command/LispCommand.java
+++ src/main/java/com/github/ashtonkem/command/LispCommand.java
@@ -1,15 +1,18 @@
 package com.github.ashtonkem.command;
 
 import java.io.File;
+import java.util.Iterator;
 
 import com.github.ashtonkem.configuration.SourceLayout;
 
 
-public interface LispCommand {
+public interface LispCommand extends Iterable<String> {
 	
 	void addExpression(String exp);
 	
-	void addFile(File f);
+	void addPackage(File f);
+	
+	void addPackage(String s);
 	
 	void setLayout(SourceLayout layout);
 	
--- src/main/java/com/github/ashtonkem/command/SBCLCommand.java
+++ src/main/java/com/github/ashtonkem/command/SBCLCommand.java
@@ -2,6 +2,7 @@ package com.github.ashtonkem.command;
 
 import java.io.File;
 import java.util.Collection;
+import java.util.Iterator;
 import java.util.LinkedList;
 
 import com.github.ashtonkem.configuration.SourceLayout;
@@ -9,28 +10,39 @@ import com.github.ashtonkem.configuration.SourceLayout;
 public class SBCLCommand implements LispCommand {
 
 	private Collection<String> expressions;
-	private SourceLayout layout;
 	private String coreName;
 	private String mainPackage;
 	private boolean packageResult;
 
 	public SBCLCommand() {
 		expressions = new LinkedList<String>();
+		this.addExpression("(require :asdf)");
 	}
 
 	public void addExpression(String exp) {
 		expressions.add(exp);
 	}
 
-	public void addFile(File f) {
+	public void addPackage(File f) {
 		if (f.exists()) {
-			this.addExpression("(load \"" + f.getAbsolutePath() + "\")");
+			this.addExpression("(pushnew (truename \"" + f.getParent() + "\") asdf::*central-registry*)");
 		}
 
 	}
+	
+	public void addPackage(String s)
+	{
+		if (s.charAt(0) == ':')
+			this.addExpression("(load " + s + ")");
+		else
+			this.addExpression("(load :" + s + ")");
+		
+	}
 
 	public void setLayout(SourceLayout s) {
-		layout = s;
+		for (File f : s.asdFiles())
+			this.addPackage(f);
+		
 	}
 
 	public String getCommand() {
@@ -38,12 +50,6 @@ public class SBCLCommand implements LispCommand {
 		for (String s : expressions) {
 			command += "\n";
 			command += s;
-		}
-		for (File f : layout.asdFiles()) {
-			if (f.exists()) {
-				command += "(pushnew (truename \"" + f.getParent()
-						+ "\") asdf::*central-registry*)\n";
-			}
 		}
 		command += "(list :output-translations :disable-cache :ignore-inherited-configuration (list *home* (merge-pathnames \"target/fasl/**/*.*\" *home*))";
 		command += "(require :" + mainPackage + ")\n";
@@ -69,5 +75,12 @@ public class SBCLCommand implements LispCommand {
 		packageResult = t;
 
 	}
+
+	public Iterator<String> iterator() {
+		// TODO Auto-generated method stub
+		return expressions.iterator();
+	}
+
+
 
 }
