c0d54d9eb66b0883be460f53eeb376a4457a0ea0
Ashton Kemerling <ashtonkemerling@gmail.com>
2012-06-25 16:19:10 -0500

    Add SBCL implementation of LispProcess

---
+++ src/main/java/com/github/ashtonkem/Processes/SBCLProcess.java
@@ -0,0 +1,23 @@
+package com.github.ashtonkem.Processes;
+
+import com.github.ashtonkem.command.LispCommand;
+
+public class SBCLProcess extends LispProcess {
+
+
+
+	@Override
+	public void stop() {
+		// TODO Auto-generated method stub
+
+	}
+
+
+
+	@Override
+	protected String getExecutableName() {
+		// TODO Auto-generated method stub
+		return "sbcl";
+	}
+
+}
