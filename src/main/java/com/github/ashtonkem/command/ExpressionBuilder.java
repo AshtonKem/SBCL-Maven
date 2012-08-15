a1b1acd260817b844b7bb54439537c121f481c3e
Ashton Kemerling <ashtonkemerling@gmail.com>
2012-08-14 22:28:21 -0500

    Expression builder class, to make life easier.

---
+++ src/main/java/com/github/ashtonkem/command/ExpressionBuilder.java
@@ -0,0 +1,49 @@
+package com.github.ashtonkem.command;
+
+import java.util.ArrayList;
+import java.util.Collection;
+
+public class ExpressionBuilder {
+	private String function;
+	private Collection<String> arguments;
+	public ExpressionBuilder(String function) {
+		this.function = function;
+		arguments = new ArrayList<String>();
+	}
+	
+	public void addKeyArgument (String key, String value) {
+		arguments.add(":" + key.toLowerCase());
+		arguments.add(value);
+	}
+	
+	public void addSymbol(String lispPackage, String value) {
+		if (lispPackage.equals(""))
+			arguments.add("'" + value);
+		else
+			arguments.add("'" + lispPackage + "::" + value);		
+	}
+	
+	public void addFunction(String lispPackage, String name) {
+		if (lispPackage.equals(""))
+			arguments.add("#'" + name);
+		else
+			arguments.add("#'" + lispPackage + "::" + name);	
+	}
+	
+	public void addList(String... items) {
+		String collector = "(list ";
+		for (String item : items)
+			collector += item + " ";
+		collector += ")";
+		arguments.add(collector);
+	}
+	
+	public String getExpression() {
+		String collector = "(";
+		collector += function;
+		for (String arg : arguments)
+			collector += arg + " ";
+		collector += ")";
+		return collector;
+	}
+}
