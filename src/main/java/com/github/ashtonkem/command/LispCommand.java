8f2a35a3f4c1256f43fedd485ec1a612d0e6adf2
Ashton Kemerling <ashtonkemerling@gmail.com>
2012-09-04 19:47:26 -0500

    No need to have the setLayout command. Since everything depends on access to a layout, make it part of the constructor, and store it internally.

--- src/main/java/com/github/ashtonkem/command/LispCommand.java
+++ src/main/java/com/github/ashtonkem/command/LispCommand.java
@@ -8,10 +8,12 @@ import com.github.ashtonkem.configuration.SourceLayout;
 public abstract class LispCommand implements Iterable<String> {
 	
 	protected boolean finalize;
+	protected SourceLayout layout;
 	
-	public LispCommand(boolean finalCommand)
+	public LispCommand(boolean finalCommand, SourceLayout layout)
 	{
 		this.finalize = finalCommand;
+		this.layout = layout;
 	}
 	
 	public boolean finalCommand() 
@@ -23,11 +25,13 @@ public abstract class LispCommand implements Iterable<String> {
 	
 	public abstract void addPackage(File f);
 	
-	public abstract void setLayout(SourceLayout layout);
-	
 	public abstract void setCoreName(String s);
 	
 	public abstract void setMainPackage(String s);
 	
 	public abstract void silenceWarnings();
+	
+	public abstract void includeFasls();
+	
+	public abstract void includeSource();
 }
